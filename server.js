3a78e54f612dc4ebe1136aa8caee66967527487d
Edward Tanoto <ed27tanoto@gmail.com>
2019-05-06 00:10:01 +0700

    test static

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 //Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use('/client/build', express.static(path.join(__dirname,'/client/build')));
+    app.use('/', express.static(path.join(__dirname,'/client/build')));
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
