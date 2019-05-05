43e6bafebed03901cb4f8efedc7c793ae6ef49d4
Edward Tanoto <ed27tanoto@gmail.com>
2019-05-05 22:58:53 +0700

    test fix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 //Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
@@ -29,6 +29,4 @@ const PORT = process.env.PORT || 5000;
 
 
 
-app.listen(PORT,()=>{
-    console.log(`Server started on port ${PORT}` );
-})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
