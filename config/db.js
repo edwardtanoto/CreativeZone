80f059d0d0aeceae6ee3cd2532ca4e4a498c5ea5
Edward Tanoto <ed27tanoto@gmail.com>
2019-05-12 20:45:21 +0700

    a

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get("mongoURI");
+const db = config.get('mongoURI');
 
 const connectDB = async() => {
     try {
        await mongoose.connect(db,{
            useNewUrlParser:true,
-           useCreateIndex : true
+           useCreateIndex : true,
+           useFindAndModify: false
        });
        console.log("MongoDB connected");
     } catch(err) {
